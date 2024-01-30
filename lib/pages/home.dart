9c499220643d833ab0dab35389a92442b80c83a0
Mahedi <mahmudrayhan256@gmail.com>
2024-01-31 02:55:17 +0600

    Fetching data from api using list

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+
+import '../models/apimodel.dart';
 
 class home extends StatefulWidget {
   const home({super.key});
@@ -11,11 +14,9 @@ class home extends StatefulWidget {
 
 class _homeState extends State<home> {
   @override
-
   final String apikey = 'https://api.github.com/search/repositories?q={flutter}{&page,per_page,sort,order}';
   bool loading = false;
-  String body = '';
-
+  List list = [];
 
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,16 +24,25 @@ class _homeState extends State<home> {
         title: Text('Flutter-Repos'),
         centerTitle: true,
       ),
-      body: Center(
-           child: Text(body),
+      body: ListView.builder(
+          itemCount: list.length,
+          itemBuilder: (context, index){
+            final repo = list[index];
+            return ListTile(
+              title: Text(repo['name']),
+              subtitle: Text(repo['full_name']),
+              trailing: Text(repo['id'].toString()),
+            );
+          },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
-          final response = await http.get(Uri.parse(apikey));
-          setState(() {
-            body = response.body;
-          });
-        },
+            final response = await http.get(Uri.parse(apikey));
+            setState(() {
+               final map = jsonDecode(response.body);
+               list = map['items'] as List;
+            });
+          },
         child: const Icon(Icons.find_in_page),
       ),
     );

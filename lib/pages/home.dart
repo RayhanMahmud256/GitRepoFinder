22e5ea2d39eaf87edac40ac4cb142def76fc669d
Mahedi <mahmudrayhan256@gmail.com>
2024-01-31 14:26:16 +0600

    Find repository using keywords

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -14,50 +14,89 @@ class home extends StatefulWidget {
 
 class _homeState extends State<home> {
   @override
+  TextEditingController _reponamecontroller = TextEditingController();
   int page = 1;
-  bool loading = true;
+  bool loading = false;
   List list = [];
   final scrollController = ScrollController();
+  String topicname = '';
 
-  @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    fetchdata();
     scrollController.addListener(_scrolllistener);
   }
 
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter-Repos'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: loading
-            ? CircularProgressIndicator()
-            : list.isEmpty
-                ? Text('Sorry we dont have any data')
-                : ListView.builder(
-                    controller: scrollController,
-                    itemCount: list.length,
-                    itemBuilder: (context, index) {
-                      final repo = list[index];
-                      return ListTile(
-                        title: Text(repo['name']),
-                        subtitle: Text(repo['full_name']),
-                        trailing: Text(repo['id'].toString()),
-                      );
+        appBar: AppBar(
+          title: Text('Github Searcher'),
+          centerTitle: true,
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(12.0),
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 15,
+              ),
+              TextField(
+                controller: _reponamecontroller,
+                decoration: InputDecoration(
+                    hintText: 'Enter the repository name',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                    )),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        list.clear();
+                        topicname = _reponamecontroller.text.trim();
+                        loading = true;
+                        fetchdata();
+                      });
                     },
-                  ),
-      ),
-    );
+                    child: const Text('Search'),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Expanded(
+                  child: loading
+                      ? CircularProgressIndicator()
+                      : list.isEmpty
+                          ? Center(child: const Text('No data found'))
+                          : ListView.builder(
+                              controller: scrollController,
+                              itemCount: list.length,
+                              itemBuilder: (context, index) {
+                                final repo = list[index];
+                                return ListTile(
+                                  title: Text(repo['name']),
+                                );
+                              }))
+            ],
+          ),
+        ));
   }
 
+
   Future<void> fetchdata() async {
-    final apikey = 'https://api.github.com/search/repositories?q=topic:ios&per_page=10&page=$page';
+    final apikey =
+        'https://api.github.com/search/repositories?q=topic:$topicname&per_page=10&page=$page';
     final response = await http.get(Uri.parse(apikey));
-    if(response.statusCode == 200) {
+    if (response.statusCode == 200) {
       final map = jsonDecode(response.body);
       setState(() {
         List ls = map['items'] as List;
